5de397b33a2491d961236f91067a32c8de10011f
yujin <yujin09219@naver.com>
2025-11-24 21:23:40 +0900

    refactor : 성공 status accepted -> ok 변경

--- Roy/umc9th/src/main/java/com/example/umc9th/global/apiPayload/code/GeneralSuccessCode.java
+++ Roy/umc9th/src/main/java/com/example/umc9th/global/apiPayload/code/GeneralSuccessCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum GeneralSuccessCode implements BaseSuccessCode {
-    SUCCESS_CODE(HttpStatus.ACCEPTED,"COMMON200","성공적으로 요청을 처리했습니다.");
+    SUCCESS_CODE(HttpStatus.OK,"COMMON200","성공적으로 요청을 처리했습니다.");
     private final HttpStatus status;
     private final String code;
     private final String message;

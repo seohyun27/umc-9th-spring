17cd30dcb3ac78d59b3282e4a2f8979260d0290e
yujin <yujin09219@naver.com>
2025-11-27 17:53:26 +0900

    fix : 권한 http 상태명 수정

--- Roy/umc9th/src/main/java/com/example/umc9th/global/validator/ManagerPermissionValidator.java
+++ Roy/umc9th/src/main/java/com/example/umc9th/global/validator/ManagerPermissionValidator.java
@@ -23,7 +23,7 @@ public class ManagerPermissionValidator implements ConstraintValidator<ManagerPe
         if (!isValid) {
             // 이 부분에서 아까 디폴트 메시지를 초기화 시키고, 새로운 메시지로 덮어씌우게 됩니다.
             context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate(MemberErrorCode.FORBIDDEN.getMessage()).addConstraintViolation();
+            context.buildConstraintViolationWithTemplate(MemberErrorCode.UNAUTHORIZED.getMessage()).addConstraintViolation();
         }
         return isValid;
     }
